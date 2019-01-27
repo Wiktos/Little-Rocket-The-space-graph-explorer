b74a7bf5c62278748930a128b0f6e0bb59ca9fee
Wiktor Łazarski <wjlazarski@gmail.com>
2019-01-27 15:35:28 +0100

    Reorganization of folders structure tests

--- tests/map/undirectedgraph_tests.h
+++ tests/map/undirectedgraph_tests.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "../source_code/map/undirected_graph.h"
-#include "../tests/error_messenger.h"
+#include "../../source_code/map/undirected_graph.h"
+#include "../error_messenger.h"
 
 namespace ugraph_test {
 
