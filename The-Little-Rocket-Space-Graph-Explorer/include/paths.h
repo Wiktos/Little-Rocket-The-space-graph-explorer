370d1d18eedbb63cdd7897a149abe1a499cd832e
Wiktor Łazarski <wjlazarski@gmail.com>
2019-02-05 17:11:20 +0100

    SOIL li updated and Model of rocket paths added

--- The-Little-Rocket-Space-Graph-Explorer/include/paths.h
+++ The-Little-Rocket-Space-Graph-Explorer/include/paths.h
@@ -1,10 +1,16 @@
 #pragma once
 #include <string>
 
-static const std::string GRAPH_NODE_VERTEX_SHADER_PATH = "source_code/visualisatioN/shaders/node_vertex.vert";
+static const std::string GRAPH_NODE_VERTEX_SHADER_PATH = "source_code/visualisation/shaders/node_vertex.vert";
 
 static const std::string GRAPH_NODE_FRAGMENT_SHADER_PATH = "source_code/visualisation/shaders/node_fragment.frag";
 
-static const std::string GRAPH_EDGE_VERTEX_SHADER_PATH = "source_code/visualisatioN/shaders/edge_vertex.vert";
+static const std::string GRAPH_EDGE_VERTEX_SHADER_PATH = "source_code/visualisation/shaders/edge_vertex.vert";
 
-static const std::string GRAPH_EDGE_FRAGMENT_SHADER_PATH = "source_code/visualisatioN/shaders/edge_fragment.frag";
+static const std::string GRAPH_EDGE_FRAGMENT_SHADER_PATH = "source_code/visualisation/shaders/edge_fragment.frag";
+
+static const std::string LITTLE_ROCKET_MODEL_OBJ_PATH = "resources/models/little_rocket/10475_Rocket_Ship_v1_L3.obj";
+
+static const std::string MODEL_VERTEX_SHADER_PATH = "source_code/visualisation/shaders/model_vertex.vert";
+
+static const std::string MODEL_FRAGMENT_SHADER_PATH = "source_code/visualisation/shaders/model_fragment.frag";
